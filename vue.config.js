fec86db17dbc05e297960738ed24996743f9077d
edwin <perchecc@163.com>
2023-05-30 01:20:47 +0800

    feat：精简编译的环境文件；更换依赖下载工具为pnpm

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    transpileDependencies: true,
-    publicPath: '/vue2-cms-template/', // TODO 自行替换为web文件部署目录
-    devServer: {
-        port: '9090',
-        proxy: {
-            '^/api/farm_cms/.*': {
-                target: process.env.VUE_APP_FARM_CMS,
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-                pathRewrite: {
-                    '^/api/': ''
-                }
-            },
-            '^/api/farm_server/.*': {
-                target: process.env.VUE_APP_FARM_SERVER,
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-                pathRewrite: {
-                    '^/api/': ''
-                }
-            },
-        }
+  transpileDependencies: true,
+  publicPath: "./", 
+  outputDir: process.env.VUE_APP_OUTPUTDIR,
+  devServer: {
+    port: "9090",
+    proxy: {
+      "^/api/.*": {
+        target: process.env.VUE_APP_API_HOST,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        pathRewrite: {
+          "^/api/": "",
+        },
+      },
     },
-})
+  },
+});
